525328b7e0e3c875c9b306e9a9ad37723330ae5e
Juan Edwards <jotaedw@MacBook-Pro-de-Jota.local>
2018-08-14 15:47:27 -0300

    Beta 0.5.3

--- components/chart.js
+++ components/chart.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
-import {BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from "recharts";
-import {getAccesToken, getRefreshToken, getMeters, getMeterStats} from "./wenuwork";
+import {BarChart, Bar, XAxis, YAxis, LabelList} from "recharts";
+import {getAccesToken, getMonthlyStats, getWeeklyStats} from "./wenuwork";
 
 const defaultData = [
       {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
@@ -43,120 +43,14 @@ export default class ChartView extends Component{
 
 	componentDidMount(){
 		const {accessToken, comparePeriod, refreshToken} = this.state;
-		var today = new Date();
-		var lastWeek = today - 6.048e+8;
-		lastWeek = new Date(lastWeek);
-		const lastWeekDay = lastWeek.getDate();
-		const thisWeekDay = today.getDate();
-		const stats = getMeterStats(this.props.sensor, comparePeriod, accessToken);
-		if (stats.status === 401) {
-			const accesReq = getRefreshToken();
-			const acTk = accesReq.access_token; 
-			const rfsTk = accesReq.refresh_token;
-			var currentEnergy = 0;
-			var currentExpent = 0;
-			var currentCo2 = 0;
-			var prevEnergy = 0;
-			var prevExpent = 0;
-			var prevCo2 = 0;
-			this.setState({accessToken: acTk, refreshToken: rfsTk});
-			this.props.sensors.forEach( (sensor) => {
-				
-				const newStats = getMeterStats(sensor, comparePeriod, acTk);
-	
-				if (lastWeekDay > thisWeekDay) {
-						
-					prevEnergy +=  newStats.data.prev.readings[lastWeekDay].ep1;
-					prevExpent +=  newStats.data.prev.readings[lastWeekDay].expent ;
-					prevCo2 += newStats.data.prev.readings[lastWeekDay].co2;
-							
-					currentEnergy += newStats.data.current.readings[thisWeekDay].ep1;
-					currentExpent += newStats.data.current.readings[thisWeekDay].expent;
-					currentCo2 += newStats.data.current.readings[thisWeekDay].co2;
-	
-				} else {
-					
-					prevEnergy = newStats.data.current.readings[lastWeekDay].ep1, 
-					prevExpent = newStats.data.current.readings[lastWeekDay].expent, 
-					prevCo2 = newStats.data.current.readings[lastWeekDay].co2
-						
-					currentEnergy = newStats.data.current.readings[thisWeekDay].ep1,
-					currentExpent = newStats.data.current.readings[thisWeekDay].expent,
-					currentCo2 = newStats.data.current.readings[thisWeekDay].co2
-					
-				}
-	
-				const sensData = [
-						{
-							name: "Previa", 
-							energy: prevEnergy / 1000, 
-							expent: prevExpent, 
-							co2: prevCo2
-						},
-						{
-							name: "Actual",
-							energy: currentEnergy / 1000,
-							expent: currentExpent,
-							co2: currentCo2
-						}
-					]
-				
-				this.setState({currentSensorsData: sensData});
-			});
-
-		} else {
-			
-			var currentEnergy = 0;
-			var currentExpent = 0;
-			var currentCo2 = 0;
-			var prevEnergy = 0;
-			var prevExpent = 0;
-			var prevCo2 = 0;
-			console.log(this.props.sensors);
-			this.props.sensors.forEach( (sensor) => {
-
-				const newStats = getMeterStats(sensor, comparePeriod, accessToken);
-					
-				if (lastWeekDay > thisWeekDay) {
-						
-					prevEnergy +=  newStats.data.prev.readings[lastWeekDay].ep1;
-					prevExpent +=  newStats.data.prev.readings[lastWeekDay].expent ;
-					prevCo2 += newStats.data.prev.readings[lastWeekDay].co2;
-							
-					currentEnergy += newStats.data.current.readings[thisWeekDay].ep1;
-					currentExpent += newStats.data.current.readings[thisWeekDay].expent;
-					currentCo2 += newStats.data.current.readings[thisWeekDay].co2;
-	
-				} else {
-					
-					prevEnergy = newStats.data.current.readings[lastWeekDay].ep1, 
-					prevExpent = newStats.data.current.readings[lastWeekDay].expent, 
-					prevCo2 = newStats.data.current.readings[lastWeekDay].co2
-						
-					currentEnergy = newStats.data.current.readings[thisWeekDay].ep1,
-					currentExpent = newStats.data.current.readings[thisWeekDay].expent,
-					currentCo2 = newStats.data.current.readings[thisWeekDay].co2
-					
-				}
-	
-				const sensData = [
-						{
-							name: "Previa", 
-							energy: prevEnergy / 1000, 
-							expent: prevExpent, 
-							co2: prevCo2
-						},
-						{
-							name: "Actual",
-							energy: currentEnergy / 1000,
-							expent: currentExpent,
-							co2: currentCo2
-						}
-					]
-				
-				this.setState({currentSensorsData: sensData});
-			});
+		let sData;
+		const {period} = this.props;
+		if(period === "mensual"){
+			sData = getMonthlyStats(this.props.sensors, accessToken, refreshToken);
+		}else if (period === "semanal"){
+			sData = getWeeklyStats(this.props.sensors, accessToken, refreshToken);
 		}
+		this.setState({currentSensorsData: sData});
 	}
 
 	render(){
@@ -166,7 +60,9 @@ export default class ChartView extends Component{
 				<BarChart width={this.props.style.width || 450} height={this.props.style.width || 400} data={currentSensorsData || defaultData}>
          			<XAxis dataKey='name'/>
          			<YAxis/>
-         			<Bar dataKey={dataKey} fill={barColor}/>
+         			<Bar dataKey={this.state.dataKey} fill={this.state.barColor}>
+						<LabelList dataKey={this.state.dataKey} position="top"/>
+					</Bar>
        			</BarChart>
        		</div>
 		)
--- components/wenuwork.js
+++ components/wenuwork.js
@@ -1,6 +1,31 @@
 
 export const domain = "https://api.wenuwork.cl/api/";
 
+const dayMap = [
+	"Domingo", 
+	"Lunes", 
+	"Martes", 
+	"Miercoles", 
+	"Jueves", 
+	"Viernes", 
+	"Sabado"
+];
+
+const monthMap = [
+	"Enero", 
+	"Febrero", 
+	"Marzo", 
+	"Abril", 
+	"Mayo", 
+	"Junio", 
+	"Julio", 
+	"Agosto", 
+	"Septiembre", 
+	"Octubre", 
+	"Noviembre", 
+	"Diciembre"
+]
+
 export function getAccesToken(){
 	var data = JSON.stringify({
 	  "username": "ld@wenuwork.com",
@@ -11,7 +36,7 @@ export function getAccesToken(){
 	});
 	
 	var xhr = new XMLHttpRequest();
-	xhr.withCredentials = false;
+	xhr.withCredentials = true;
 	
 	xhr.open("POST", "https://api.wenuwork.cl/api/auth/oauth/token", null);
 	xhr.setRequestHeader("Content-Type", "application/json");
@@ -33,7 +58,7 @@ export function getRefreshToken(rToken){
 	});
 	
 	var xhr = new XMLHttpRequest();
-	xhr.withCredentials = false;
+	xhr.withCredentials = true;
 
 	xhr.open("POST", "https://api.wenuwork.cl/api/auth/oauth/token", null);
 	xhr.setRequestHeader("Content-Type", "application/json");
@@ -48,7 +73,7 @@ export function getMeters( aToken){
 	var data = null;
 
 	var xhr = new XMLHttpRequest();
-	xhr.withCredentials = false;
+	xhr.withCredentials = true;
 	
 	
 	xhr.open("GET", "https://api.wenuwork.cl/api/companies/5995a9711bf1469ec8b6612c", null);
@@ -67,13 +92,127 @@ export function getMeterStats(meter, period, aToken){
 	var data = null;
 
 	var xhr = new XMLHttpRequest();
-	xhr.withCredentials = false;
-	
-	xhr.open("GET", "https://api.wenuwork.cl/api/meters/"+meter+"/stats?type=monthly", null);
+	xhr.withCredentials = true;
+	xhr.onreadystatechange = function () {
+		if(xhr.status === 401 ){
+			return {data: null, status: 401};
+		}
+	}
+	xhr.open("GET", "https://api.wenuwork.cl/api/meters/"+meter+"/stats?type=" + period, null);
 	xhr.setRequestHeader("Authorization", "Bearer " + aToken);
 	xhr.setRequestHeader("Cache-Control", "no-cache");
 	
 	xhr.send(data);
 	
 	return {data: JSON.parse(xhr.responseText), status: xhr.status}
+}
+
+export function getWeeklyStats(sensors, aToken, rToken){
+	var today = new Date();
+	var lastWeek = today - 6.048e+8;
+	lastWeek = new Date(lastWeek);
+	const lastWeekDay = lastWeek.getDate();
+	const thisWeekDay = today.getDate();
+	const weekDay = dayMap[today.getDay()]
+	var currentEnergy = 0;
+	var currentExpent = 0;
+	var currentCo2 = 0;
+	var prevEnergy = 0;
+	var prevExpent = 0;
+	var prevCo2 = 0;
+	sensors ? sensors.forEach( (sensor) => {
+		let stats = aToken ? getMeterStats(sensor, "monthly", aToken) : null;
+		if(stats.status === 401){
+			const refreshReq = getRefreshToken(rToken);
+			localStorage.setItem("access_token", refreshReq.access_token);
+			localStorage.setItem("refresh_token", refreshReq.refresh_token);
+			stats = getMeterStats(sensor, "monthly", refreshreq.access_token);
+		}
+
+		if (lastWeekDay > thisWeekDay) {
+						
+			prevEnergy +=  stats.data.prev.readings[lastWeekDay].ep1;
+			prevExpent +=  stats.data.prev.readings[lastWeekDay].expent ;
+			prevCo2 += stats.data.prev.readings[lastWeekDay].co2;
+						
+			currentEnergy += stats.data.current.readings[thisWeekDay].ep1;
+			currentExpent += stats.data.current.readings[thisWeekDay].expent;
+			currentCo2 += stats.data.current.readings[thisWeekDay].co2;
+
+		} else {
+					
+			prevEnergy += stats.data.current.readings[lastWeekDay].ep1, 
+			prevExpent += stats.data.current.readings[lastWeekDay].expent, 
+			prevCo2 += stats.data.current.readings[lastWeekDay].co2
+						
+			currentEnergy += stats.data.current.readings[thisWeekDay].ep1,
+			currentExpent += stats.data.current.readings[thisWeekDay].expent,
+			currentCo2 += stats.data.current.readings[thisWeekDay].co2
+					
+		}
+
+	}) : null;
+	const sensData = [
+		{
+			name: weekDay + " Pasado", 
+			energy: Math.round((prevEnergy / 1000) * 100) / 100, 
+			expent: Math.round(prevExpent * 100) / 100, 
+			co2: Math.round(prevCo2 * 100) / 100
+		},
+		{
+			name: weekDay + " Actual",
+			energy: Math.round((currentEnergy / 1000) * 100) / 100,
+			expent: Math.round(currentExpent * 100) / 100,
+			co2: Math.round(currentCo2 * 100) / 100 
+		}
+	]
+
+	return sensData;
+}
+
+export function getMonthlyStats(sensors, aToken, rToken){
+	var today = new Date();
+	var monthDay = today.getMonth() + 1;
+	const dayString = monthMap[monthDay - 1];
+	var currentEnergy = 0;
+	var currentExpent = 0;
+	var currentCo2 = 0;
+	var prevEnergy = 0;
+	var prevExpent = 0;
+	var prevCo2 = 0;
+	sensors.forEach( (sensor) => {
+		let stats = aToken ? getMeterStats(sensor, "annual", aToken) : null;
+		if(stats.status === 401){
+			const refreshReq = getRefreshToken(rToken);
+			localStorage.setItem("access_token", refreshReq.access_token);
+			localStorage.setItem("refresh_token", refreshReq.refresh_token);
+			stats = getMeterStats(sensor, "annual", refreshreq.access_token);
+		}
+
+						
+		prevEnergy +=  stats.data.prev.readings[monthDay].ep1;
+		prevExpent +=  stats.data.prev.readings[monthDay].expent ;
+		prevCo2 += stats.data.prev.readings[monthDay].co2;
+					
+		currentEnergy += stats.data.current.readings[monthDay].ep1;
+		currentExpent += stats.data.current.readings[monthDay].expent;
+		currentCo2 += stats.data.current.readings[monthDay].co2;
+
+	});
+	const sensData = [
+		{
+			name: dayString + " Previo", 
+			energy: Math.round((prevEnergy / 1000) * 100) / 100, 
+			expent: Math.round(prevExpent * 100) / 100, 
+			co2: Math.round(prevCo2 * 100) / 100
+		},
+		{
+			name: dayString + " Actual",
+			energy: Math.round((currentEnergy / 1000) * 100) / 100,
+			expent: Math.round(currentExpent * 100) / 100,
+			co2: Math.round(currentCo2 * 100) / 100 
+		}
+	]
+
+	return sensData;
 }
